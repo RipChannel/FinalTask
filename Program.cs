031073fdfcaff83d8b816fe59a6dab020991b237
Andrey <mr.super.gorn@mail.ru>
2022-09-19 22:50:56 +1000

    Вводится первый массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите колличество данных");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+string[] firstArr = new string[arraySize];
+
+for(int i = 0; i < arraySize; i++)
+{
+    string input = Console.ReadLine();
+    if(!String.IsNullOrEmpty(input))
+    firstArr[i] = input;
+    else
+    {
+         Console.WriteLine("Неккоректные данные");
+         break;
+    }
+}
