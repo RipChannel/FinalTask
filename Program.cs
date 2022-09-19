aa73730089ecfde02afb7a0f32bcfe663b97b81e
Andrey <mr.super.gorn@mail.ru>
2022-09-19 23:08:54 +1000

    Вывод массива в консоль

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("Введите колличество данных");
 int arraySize = Convert.ToInt32(Console.ReadLine());
+string[] firstArray = FirstArray(arraySize);
+PrintArr(firstArray);
 
+string[] FirstArray(int arrSize)
+{
 string[] firstArr = new string[arraySize];
-
 for(int i = 0; i < arraySize; i++)
 {
     string input = Console.ReadLine();
@@ -10,7 +13,18 @@ for(int i = 0; i < arraySize; i++)
     firstArr[i] = input;
     else
     {
-         Console.WriteLine("Неккоректные данные");
-         break;
+         Console.WriteLine("Нельзя вводить пустые строки, введите значение заново");
+         i--;
+    }
+}
+return firstArr;
+}
+void PrintArr(string[] Arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < Arr.GetLength(0); i++)
+    {
+        if(i < Arr.GetLength(0) - 1) Console.Write($"{Arr[i]}, ");
+        else Console.Write($"{Arr[i]}]");
     }
 }
